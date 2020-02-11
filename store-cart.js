a15ad9d6c05d76382f4f87e73d26092abaeda17c
diegof95 <diego.botero@correounivalle.edu.co>
2020-02-10 21:41:01 -0500

    Store ahora carga JS en defer. Funcionalidad de agregar elementos de la tienda al carrito de compras.

--- store-cart.js
+++ store-cart.js
@@ -27,11 +27,7 @@ function init() {
     let btn = removeBtns[n];
     btn.addEventListener(
       'click',
-      (event) => {
-        let clickedBtn = event.target;
-        clickedBtn.parentElement.parentElement.remove();
-        updateCartTotal();
-      }
+      removeItem
     );
   }
 }
@@ -40,9 +36,57 @@ function buyCart(){
   console.log("Comprar carro");
 }
 
-function addCartItem(){
-  console.log("Added to cart");
+function addCartItem(event){
+  let cartItems = document.getElementsByClassName("cart-item");
+  let itemAdd = event.target.parentElement.parentElement;
+  let itemAddName = itemAdd.children[0].innerText;
+
+  let duplicatedItem = false;
+  for(let item of cartItems){
+    let itemName = item.children[0].children[1].innerText;
+    if(itemName == itemAddName) {
+      window.alert("The item is already on the cart")
+      duplicatedItem = true;
+    }
+  }
+  if(!duplicatedItem){
+    newCartItem(itemAdd, itemAddName);
+    updateCartTotal();
+  }
 }
+
+function newCartItem(itemAdd, itemAddName) {
+  let itemRow = document.createElement("tr");
+  itemRow.classList.add("cart-item");
+  let itemAddImg = itemAdd.children[1].getAttribute("src");
+  let itemAddPrice = itemAdd.children[2].children[0].innerText;
+  let cartItemInfo =
+      `<td class="cart-item-info">
+          <img class="cart-item-img" src="${itemAddImg}"
+                alt="${itemAddName}" width="100" height="100" />
+          <span class="cart-item-title">${itemAddName}</span>
+        </td>
+        <td class="cart-item-price">${itemAddPrice}</td>
+        <td><input type="number" class="cart-item-quant" name="agregar_cantidad" value="1" min="0"></td>
+        <td><button class="btn btn-danger">Remover</button></td>`;
+  itemRow.innerHTML = cartItemInfo;
+  let cartTable = document.querySelector(".cart-table tbody");
+  cartTable.appendChild(itemRow);
+  itemRow.getElementsByClassName("cart-item-quant")[0].addEventListener(
+      'change',
+      updateCartTotal
+    );
+  itemRow.getElementsByClassName("btn-danger")[0].addEventListener(
+      'click',
+      removeItem
+    );
+}
+
+const removeItem = (event) => {
+        let clickedBtn = event.target;
+        clickedBtn.parentElement.parentElement.remove();
+        updateCartTotal();
+      }
 
 function updateCartTotal() {
   let cartItems = document.getElementsByClassName("cart-item");
